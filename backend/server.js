ce60c47c039e1333bb1c0fe32c6df6304e5a2067
Musa Khan <16khanmusa@gmail.com>
2026-01-02 20:38:33 +0530

    fixing error

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
+const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
